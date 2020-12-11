86d1188f2703ef4c9209fc4e41502b1b0915fd0b
eneserdo1 <eneserdogan.66@outlook.com>
2020-12-11 13:55:41 +0300

    Searchview Eklendi

---
+++ app/src/main/java/com/eneserdogan/hizligeliyocase/model/RecyclerModel.kt
@@ -0,0 +1,8 @@
+package com.eneserdogan.hizligeliyocase.model
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class RecyclerModel(application: Application) : AndroidViewModel(application) {
+
+}
